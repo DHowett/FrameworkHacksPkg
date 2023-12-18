09261d50d4164cb0fd6634fb563fd6bceac2ff5c
Dustin L. Howett <dustin@howett.net>
2023-12-17 20:13:20 -0600

    FmapLib: format with clang-format

--- Library/FmapLib/Fmap.c
+++ Library/FmapLib/Fmap.c
@@ -1,8 +1,8 @@
 #include <Uefi.h>
 
 #include <Library/BaseMemoryLib.h>
-#include <Library/UefiLib.h>
 #include <Library/FmapLib.h>
+#include <Library/UefiLib.h>
 
 EC_IMAGE_FMAP_HEADER* GetImageFlashMap(const VOID* Buffer, UINTN Length) {
 	return (EC_IMAGE_FMAP_HEADER*)ScanMem64(Buffer, Length, 0x5F5F50414D465F5FULL /* __FMAP__, little-endian */);
@@ -11,7 +11,7 @@ EC_IMAGE_FMAP_HEADER* GetImageFlashMap(const VOID* Buffer, UINTN Length) {
 EC_IMAGE_FMAP_AREA_HEADER* GetImageFlashArea(EC_IMAGE_FMAP_HEADER* Map, CHAR8* AreaName) {
 	EC_IMAGE_FMAP_AREA_HEADER* FmapAreas = (EC_IMAGE_FMAP_AREA_HEADER*)(Map + 1);
 	for(int i = 0; i < Map->NAreas; ++i) {
-		if (0 == AsciiStrnCmp(AreaName, FmapAreas[i].Name, FMAP_NAMELEN)) {
+		if(0 == AsciiStrnCmp(AreaName, FmapAreas[i].Name, FMAP_NAMELEN)) {
 			return FmapAreas + i;
 		}
 	}
